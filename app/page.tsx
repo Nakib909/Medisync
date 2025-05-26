3088fdcbebebfe1ddd47a3e9642b52aaa6e84864
Nakib <nakibbhaiya1@gmail.com>
2025-05-26 11:57:22 +0600

    home page started

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl underline">Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <div className="w-fit mb-12 flex flex-row items-center justify-center gap-2">
+            <Image
+              src="/assets/icons/logo-icon.svg"
+              height={1000}
+              width={1000}
+              alt="logo"
+              className=" h-10 w-fit"
+            />
+            <div className="text-lg text-white font-bold">MediSync</div>
+          </div>
+        </div>
+
+      </section>
     </div>
   );
 }
