59c13176ed246e27ed3ca012a21d1d5eb68a7590
Nakib <nakibbhaiya1@gmail.com>
2025-05-27 20:38:12 +0600

    latest update on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import PatientForm from "@/components/forms/patientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -15,9 +17,29 @@ export default function Home() {
             />
             <div className="text-lg text-white font-bold">MediSync</div>
           </div>
+
+          <PatientForm/>
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2025 MediSync. All rights reserved.
+            </p> 
+
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
 
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
